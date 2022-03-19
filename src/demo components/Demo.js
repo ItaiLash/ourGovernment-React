e9b5f2accf10fe52cc58e361952febef81c8bcca
amichaikafka <amichaikp@gmail.com>
2022-03-20 00:01:05 +0200

    connect to front

--- src/demo components/Demo.js
+++ src/demo components/Demo.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React ,{useEffect,useState}from "react";
 
 function Demo() {
-  return <div>Demo</div>;
+
+  const [pav,setResult]=useState([]);
+
+  useEffect(()=>{
+    fetch("http://127.0.0.1:8000/api/pav/0/compute_pav",{
+      method:'GET',
+  
+    })
+    .then(resp => resp.json())
+    .then(resp => setResult(resp))
+    // .then(resp => console.log(resp.result))
+    
+
+  },[])
+  return (
+  <div>
+    <div>Demo</div>
+    <div>
+      {pav.result}
+    </div>
+  </div>
+  );
 }
 
 export default Demo;
