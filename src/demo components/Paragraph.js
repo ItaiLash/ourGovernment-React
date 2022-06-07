7a527afcf9a73a841a369ce12b65feb6017da635
amichaikafka <amichaikp@gmail.com>
2022-06-07 21:41:54 +0300

    change demo Paragraph

--- src/demo components/Paragraph.js
+++ src/demo components/Paragraph.js
@@ -5,12 +5,6 @@ import style from "./style_demo.module.css";
 function Paragraph() {
   return (
     <p className={style.headerText}>
-      The executive branch, or government, is typically not elected directly by
-      the people, but rather formed by another elected body or person such as
-      the parliament or the president.
-      <br />
-      As a result, it's members are not directly accountable to the people,
-      individually or as a group.
       <br />
       Here you can create your own government by using the GreedyPAV algorithm (greedy variant of Proportional Approval Voting).
       <br />
