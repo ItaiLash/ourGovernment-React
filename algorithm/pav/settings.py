6dcec454f6c38b696e030e652794448700b36df4
amichaikafka <amichaikp@gmail.com>
2022-06-08 12:27:17 +0300

    heroku

--- algorithm/pav/settings.py
+++ algorithm/pav/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://our-government-ariel.herokuapp.com/']
 
 # Application definition
 
