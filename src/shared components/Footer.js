c08a0c598caf82e2da121babb61d8e7f6886e6fd
itai <itai.lash@gmail.com>
2022-06-08 16:18:11 +0300

    Paragraphs and Footer update

--- src/shared components/Footer.js
+++ src/shared components/Footer.js
@@ -4,7 +4,7 @@ import style from "./style_footer.module.css";
 function Footer() {
   return (
     <footer classsName={style.footer}>
-      Copyright &copy; 2021 by Itai Lashover. Part of "Computer science course
+      Copyright &copy; 2022 by Itai Lashover, liav Weiss, Amichai Kafkd annd Shani Levin. Part of "Computer science course
       at Ariel University". Use for learning purposes only.
     </footer>
   );
