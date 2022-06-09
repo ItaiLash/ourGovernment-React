007e621f32365164a63fd12357df2f94e1fa824e
amichaikafka <amichaikp@gmail.com>
2022-06-09 18:17:36 +0300

    just try somthing

--- src/demo components/FormBox.js
+++ src/demo components/FormBox.js
@@ -406,7 +406,7 @@ class FormBox extends React.Component {
    * this finction is sending http post requst to the Django server api
    */
   callToPav = async () => {
-    await fetch("http://127.0.0.1:8000/api/pav/0/compute_pav/", {
+    await fetch("https://our-government-ariel.herokuapp.com/api/pav/0/compute_pav/", {
       method: "POST",
       headers: {
         Accept: "application/json",
@@ -431,7 +431,7 @@ class FormBox extends React.Component {
   handlePDFDownload = (e) => {
     e.preventDefault();
     Axios({
-      url: "http://127.0.0.1:8000/api/pav/0/download_results/",
+      url: "https://our-government-ariel.herokuapp.com/api/pav/0/download_results/",
       method: "GET",
       responseType: "blob",
     }).then((res) => {
