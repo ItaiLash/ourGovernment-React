c6096c07cd5b4ea4e9020cfa2f883bb205f0b9e4
itai <itai.lash@gmail.com>
2022-06-14 23:56:36 +0300

    HashRouter for deploy

--- src/App.js
+++ src/App.js
@@ -3,19 +3,19 @@ import Home from './home components/Home'
 import Demo from "./demo components/Demo";
 import UploadFile from "./upload file components/UploadFile";
 import About from "./about components/About";
-import { Routes, Route } from "react-router-dom";
+import { HashRouter, Route } from "react-router-dom";
 import Navbar from "./Navigation/Navbar";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Routes>
-        <Route exact path="/" element={<Home />} />
+      <HashRouter>
+        <Route exact pasth="/" element={<Home />} />
         <Route exact path="/demo" element={<Demo />} />
         <Route exact path="/upload-file" element={<UploadFile />} />
         <Route exact path="/about" element={<About />} />
-      </Routes>
+      </HashRouter>
     </div>
   );
 }
