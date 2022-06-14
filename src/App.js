8a96fd9c40eada88a9b0a727e945a04cace5793a
itai <itai.lash@gmail.com>
2022-06-15 00:13:21 +0300

    Routes

--- src/App.js
+++ src/App.js
@@ -3,19 +3,19 @@ import Home from './home components/Home'
 import Demo from "./demo components/Demo";
 import UploadFile from "./upload file components/UploadFile";
 import About from "./about components/About";
-import { HashRouter, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./Navigation/Navbar";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <HashRouter>
-        <Route exact pasth="/" element={<Home />} />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
         <Route exact path="/demo" element={<Demo />} />
         <Route exact path="/upload-file" element={<UploadFile />} />
         <Route exact path="/about" element={<About />} />
-      </HashRouter>
+      </Routes>
     </div>
   );
 }
