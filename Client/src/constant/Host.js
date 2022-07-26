d4278273c60ea50578bf223ff46d234480a0eae5
itai <itai.lash@gmail.com>
2022-07-26 22:37:03 +0300

    Reconstruct project

---
+++ Client/src/constant/Host.js
@@ -0,0 +1,2 @@
+export const HOST = "http://127.0.0.1:8000";
+// export const HOST = "https://our-government-server2.herokuapp.com";
--- src/constant/Host.js
+++
@@ -1,2 +0,0 @@
-// export const HOST = "http://127.0.0.1:8000";
-export const HOST = "https://our-government-server2.herokuapp.com";
